ef148d48e9952cd597e2a704e6b5d7bd6475b694
Arun <rarnkmrbridgelabz.com>
2022-03-31 19:43:51 +0530

    Added UC-8

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -6,11 +6,7 @@ public class LinkedListMain {
         list.add(56);
         list.add(30);
         list.add(70);
+        list.insertAfter(30, 40);
         list.display();
-        list.search(30);
-        if (list.search(30) == null)
-            System.out.println("\nData Not Found in the Linked List");
-        else
-            System.out.println("\nData Found in the Linked List");
     }
 }
