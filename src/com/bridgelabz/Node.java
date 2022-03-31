6f72e06da7fce7de3419eaac7a9943fbd793b081
Arun <rarnkmrbridgelabz.com>
2022-03-31 19:04:55 +0530

    Added UC-1

---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+public class Node {
+        int key;
+        Node next;
+
+        public Node(int key) {
+            this.key = key;
+            this.next = null;
+        }
+    }
